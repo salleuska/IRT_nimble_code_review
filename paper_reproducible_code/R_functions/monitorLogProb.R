dba2bc4fee0c8445af6b988ff365d86b7aca4a9d
salleuska <sallypaganin@gmail.com>
2022-02-18 17:10:39 -0500

    sampler to monitor posterior logProbs

--- paper_reproducible_code/R_functions/monitorLogProb.R
+++ paper_reproducible_code/R_functions/monitorLogProb.R
@@ -1,10 +1,8 @@
 #############
 ## Custom sampler to monitor posterior logProbabilities
 ############# 
-library(nimble)
-############# 
 
-## sampler to monitor 
+## sampler to monitor posterior log-probabilities 
 logProb_summer <- nimbleFunction(
 	name = 'logProb_summer',
 	contains = sampler_BASE,
@@ -22,28 +20,28 @@ logProb_summer <- nimbleFunction(
     run = function() {
         model[[target]] <<- model$getLogProb(nodes)  
         
-    # copy(from = model, to = mvSaved, 
-    # 	row = 1, nodes = target, logProb = TRUE)
+    copy(from = model, to = mvSaved, 
+    	row = 1, nodes = target, logProb = TRUE)
    }, 
    methods = list( reset = function () {} )
 )
  
-logLik_summer <- nimbleFunction(
-	name = 'logLik_summer',
-	contains = sampler_BASE,
-    setup = function(model, mvSaved, target, control) {
-          dataNodes <- model$getNodeNames(dataOnly = TRUE)
-          dataNodes <- dataNodes[dataNodes != target]
-    },
-    run = function() {
-        model[[target]] <<- model$getLogProb(dataNodes)  
+# logLik_summer <- nimbleFunction(
+# 	name = 'logLik_summer',
+# 	contains = sampler_BASE,
+#     setup = function(model, mvSaved, target, control) {
+#           dataNodes <- model$getNodeNames(dataOnly = TRUE)
+#           dataNodes <- dataNodes[dataNodes != target]
+#     },
+#     run = function() {
+#         model[[target]] <<- model$getLogProb(dataNodes)  
 
-        copy(from = model, to = mvSaved, 
-    	row = 1, nodes = target, logProb = TRUE)
+#         copy(from = model, to = mvSaved, 
+#     	row = 1, nodes = target, logProb = TRUE)
 
-   }, 
-   methods = list( reset = function () {})
-)
+#    }, 
+#    methods = list( reset = function () {})
+# )
  
 
 # # monitor logProb_sum
@@ -51,87 +49,84 @@ logLik_summer <- nimbleFunction(
 # conf$addSampler("logProb_sum", type =  "logProb_summer", 
 # 				control = list(nodeList = c("beta", "lambda", "eta")) )
 
-data <- list(y = readRDS("data/simulation_bimodal.rds"))
+# data <- list(y = readRDS("data/simulation_bimodal.rds"))
 
-###------------------------------------------------ ##
-## Parametric 2PL - constraints on abilities ----
-##------------------------------------------------##
-code2PL <- nimbleCode({
-  for(i in 1:I) {
-    for(j in 1:N) {
-      y[j, i] ~ dbern(pi[j, i])
-      logit(pi[j, i]) <-  lambda[i]*(eta[j] - beta[i])
-    }
-  }  
+# ###------------------------------------------------ ##
+# ## Parametric 2PL - constraints on abilities ----
+# ##------------------------------------------------##
+# code2PL <- nimbleCode({
+#   for(i in 1:I) {
+#     for(j in 1:N) {
+#       y[j, i] ~ dbern(pi[j, i])
+#       logit(pi[j, i]) <-  lambda[i]*(eta[j] - beta[i])
+#     }
+#   }  
   
-  for(i in 1:I) {
-    log(lambda[i]) ~ dnorm(0.5, var = 0.5)   
-    beta[i] ~ dnorm(0,  var = 3)
-  } 
+#   for(i in 1:I) {
+#     log(lambda[i]) ~ dnorm(0.5, var = 0.5)   
+#     beta[i] ~ dnorm(0,  var = 3)
+#   } 
   
-  for(j in 1:N) {
-    eta[j] ~ dnorm(0, 1)
-  }
+#   for(j in 1:N) {
+#     eta[j] ~ dnorm(0, 1)
+#   }
 
-  # ## dummy nodes to track log porbability and log likelihood
-  logProbAll  ~ dnorm(0,1)
-  # logProbSum ~ dnorm(0,1)
-  # logLik      ~ dnorm(0,1)
+#   # ## dummy nodes to track log porbability and log likelihood
+#   myLogProbAll  ~ dnorm(0,1)
+#   myLogProbSome ~ dnorm(0,1)
+#   myLogLik      ~ dnorm(0,1)
+# })
 
-})
+# constants <- list(I= dim(data$y)[2], N = dim(data$y)[1])
 
-constants <- list(I= dim(data$y)[2], N = dim(data$y)[1])
+# inits <- list(beta   = rnorm(constants$I, 0, 1),
+#               log_lambda = runif(constants$I, -1, 1))
 
-inits <- list(beta   = rnorm(constants$I, 0, 1),
-              log_lambda = runif(constants$I, -1, 1))
+# inits$lambda <- exp(inits$log_lambda)
 
-inits$lambda <- exp(inits$log_lambda)
-
-monitors <- c("beta", "lambda")
-
-
-model <- nimbleModel(code  = code2PL,
-					data 			= data,  
-					constants	= constants,
-					inits 			= inits, 
-					calculate 	= FALSE)
-
-# monitors <- c(monitors, "logProbAll", "logProbSum", "logLik")
-monitors <- c(monitors, "logProbAll")
-
-mcmcConf <- configureMCMC(model, monitors = monitors)
-
-# mcmcConf$removeSampler("logLik")
-# mcmcConf$addSampler("logLik", type =  "logLik_summer")
-mcmcConf$removeSampler("logProbAll")
-mcmcConf$addSampler("logProbAll", type =  "logProb_summer")
-
-# mcmcConf$addSampler("logProbSum", type =  "logProb_summer", 
-# control = list(nodeList = c("beta", "lambda", "eta")))
-mcmcConf
-
-mcmc <- buildMCMC(mcmcConf)	
-
-mcmc$run(1)
-
-Cmodel <- compileNimble(model)
-Cmcmc <- compileNimble(mcmc, project = model, 
-  showCompilerOutput = TRUE)
-
-# Error in eval(call(".Call", nimbleUserNamespace$sessionSpecificDll$populateValueMapAccessorsFromNodeNames,  :
-#   VECTOR_ELT() can only be applied to a 'list', not a 'NULL'
-
-res <- runMCMC(Cmcmc, 
-		   niter 	= 1000,  
-		   nburnin  = 100, 
-		   setSeed  = 32)
-
-colnames(res)
-res[, "logProbAll"]
+# monitors <- c("beta", "lambda")
 
 
-res[, "logProS"]
-res[, "logLik"]
+# model <- nimbleModel(code  = code2PL,
+# 					data 			= data,  
+# 					constants	= constants,
+# 					inits 			= inits, 
+# 					calculate 	= FALSE)
+
+# monitors <- c(monitors, "myLogProbAll", "myLogProbSome", "myLogLik")
+# # monitors <- c(monitors, "myLogProbAll")
+
+# mcmcConf <- configureMCMC(model, monitors = monitors)
+
+# mcmcConf$removeSampler("myLogLik")
+# mcmcConf$addSampler("myLogLik", type =  "logProb_summer", 
+#   control = list(nodeList = c("y")))
+
+# mcmcConf$removeSampler("myLogProbAll")
+# mcmcConf$addSampler("myLogProbAll", type =  "logProb_summer")
+
+# mcmcConf$removeSampler("myLogProbSome")
+# mcmcConf$addSampler("myLogProbSome", type =  "logProb_summer", 
+#   control = list(nodeList = c("beta", "lambda", "eta")))
+# mcmcConf
+
+# mcmc <- buildMCMC(mcmcConf)	
+
+# Cmodel <- compileNimble(model)
+# Cmcmc <- compileNimble(mcmc, project = model)
+
+
+# res <- runMCMC(Cmcmc, 
+# 		   niter 	= 1000,  
+# 		   nburnin  = 100, 
+# 		   setSeed  = 32)
+
+# colnames(res)
+# res[, "myLogProbAll"]
+# res[, "myLogProbSome"]
+# res[, "myLogLik"]
+
+
 
 
 
